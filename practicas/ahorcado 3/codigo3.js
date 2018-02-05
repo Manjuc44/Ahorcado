7b095f2311978e1e9c37cc4c547e311d1a04f730
ALUMNADO\maceas <Manjuc4499@gmail.com>
2018-02-05 13:28:12 +0100

    no message

--- practicas/ahorcado 3/codigo3.js
+++ practicas/ahorcado 3/codigo3.js
@@ -8,36 +8,45 @@ var palabra0=[];
 var  condicion=true;
 var conterror=0;
 var contacert=0;
+var boton=document.getElementById("button");
 
 console.log(palabra0)
 for (var i = 0; i < adivinar.length; i++) {
   palabra0[i]="0";
 }
 
-while (conterror<5 && contacert<adivinar.length) {
-  var letrauser=prompt("Dame una letra");
-  var condicion=false
-  for (var i = 0; i < adivinar.length; i++) {
-    if (letrauser==adivinar.charAt(i)){
-        palabra0[i]=letrauser
-        condicion=true;
-        contacert++;
-      }     else {
-
-}
-}
-      if (condicion) {
-            alert("Has acertado")
-
+  function mostrarMensaje() {
+      var acert=false;
+      var usertext="";
+      usertext = letrauser.value;
+        if (isNaN(usertext)) {
+          for (var i = 0; i < adivinar.length; i++) {
+            if (adivinar.charAt(i)==usertext) {
+              palabra0[i]=adivinar.charAt(i);
+              acert=true;
+             contacert++;
           }
-
-          else {
-            alert("Has fallado")
-            conterror++
+          salida2.value=palabra0;
+          if (contacert>=adivinar.length) {
+                salida2.value="Felicidades has acertado la palabra y es: "+adivinar
+                boton=document.getElementById("button").disabled=true;
+          } else if (conterror>=5) {
+                salida2.value="No lo has conseguido..."
+                boton=document.getElementById("button").disabled=true;
           }
+              letrauser.value="";
+          }
+          if (acert==true) {
+                salida.value="Has acertado la letra";
+          } else {
+                salida.value="No has acertado";
+                conterror++;
+                salida3.value=salida3.value+usertext+"-";
+          }
+           console.log(palabra0);
+           console.log(conterror);
+        }  else {
+              salida.value="Una letra por favor";
 
-
-  }
-console.log(palabra0)
-console.log(contacert)
-console.log(conterror)
+        }
+    }
